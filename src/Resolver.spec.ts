9c03476bf7df891ae4135e46cdad74db92e28cf8
Laci Szűcs <ifrozen@gmail.com>
2018-07-18 13:14:59 +0200

    Test Resolver (abstract) class directly

--- src/Resolver.spec.ts
+++ src/Resolver.spec.ts
@@ -2,6 +2,7 @@ import { InlineResolver, Resolver } from './Resolver';
 
 describe('InlineResolver', () => {
   it('should be a Resolver', () => {
+    expect(new Resolver()).toBeInstanceOf(Resolver);
     expect(new InlineResolver()).toBeInstanceOf(Resolver);
   });
 
