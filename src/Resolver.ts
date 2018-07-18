0e69f3fa2b6d62293dd063fd82cafb86fa4e48b5
Laci Szűcs <ifrozen@gmail.com>
2018-07-18 11:40:19 +0200

    Document some functions

--- src/Resolver.ts
+++ src/Resolver.ts
@@ -3,6 +3,8 @@ type ResolverFn =
   
 /**
  * Represents one resolver in the resolvers object given to makeExecutableSchema.
+ * @property {string} key - Dot-notation key of the resolver
+ * @property {ResolverFn} resolver - The value to be injected into the final resolvers object
  */
 export abstract class Resolver {
   public abstract key: string;
@@ -11,6 +13,10 @@ export abstract class Resolver {
     return this.resolver;
   }
 }
+
+// export class Query extends Resolver {}
+// export class Mutation extends Resolver {}
+// export class Subscription extends Resolver {}
 
 export class InlineResolver extends Resolver {
   public key = `Mutation`;
--- src/Resolvers.ts
+++ src/Resolvers.ts
@@ -5,6 +5,12 @@ import { set } from "lodash";
  * Reduces all Resolver instances into a nested object
  */
 export class Resolvers {
+  /** 
+   * Transform (purely) an array of Resolver instances to 
+   * a nested object which can be fed to makeExecutableSchema.
+   * 
+   * @param {Resolver[]} resolvers - Array of Resolver instances to transform
+   */
   private static toResolversObject(resolvers: Resolver[]):object {
     return resolvers.reduce((resolversObject, resolver:Resolver) => {
       return set(resolversObject, resolver.key, resolver.toResolver());
