88eecc4973636e3c7a125b8db4245187055a7fcb
Laci Szűcs <ifrozen@gmail.com>
2018-07-20 04:01:51 +0200

    Handle missing Subscription.topic

--- src/Subscription.ts
+++ src/Subscription.ts
@@ -5,13 +5,28 @@ import {Resolver} from './Resolver';
 export abstract class Subscription extends Resolver {
   public abstract filter: FilterFn;
   public pubsub: PubSub;
+  public abstract topic?: string;
 
   constructor(pubsub: PubSub) {
     super();
     this.pubsub = pubsub;
   }
   public toResolver(): ResolverFn {
-    // if (!this.key.endsWith('.subscribe')) this.key = `${this.key}.subscribe`;
-    return withFilter(this.pubsub.asyncIterator(this.key), this.filter);
+    this.validateKey(this.key);
+    return withFilter(this.pubsub.asyncIterator(this.getTopic()), this.filter);
+  }
+
+  public getTopic(): string {
+    return this.topic || this.key;
+  }
+
+  private validateKey(key: string) {
+    if (typeof key !== 'string') {
+      throw new Error(`Subscription key is required, got ${typeof key}.`);
+    } else if (key.endsWith('.subscribe')) {
+      return key;
+    } else {
+      throw new Error(`Subscription key (${key}) does not end with ".subscribe".`);
+    }
   }
 }
